d0b589cc751b64f32946eca445df918ad8589f3f
edu2ucoc <edu2.ucoc@gmail.com>
2025-12-23 15:34:34 +0900

    feat: llm 구성 및 외부도구 설정중

--- agent_with_graph.py
+++ agent_with_graph.py
@@ -11,6 +11,16 @@ from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage, BaseMessage
 from langchain_aws import ChatBedrockConverse, ChatBedrock
 from langgraph.prebuilt import ToolNode, tools_condition
+from tools import rag_search
+
+# 1. LLM 모델 구성, client 생략
+llm = ChatBedrockConverse(model      = os.getenv('BEDROCK_MODEL_ID'),
+                         region_name = os.getenv('AWS_REGION'),
+                         temperature = 0.5,
+                         max_tokens  = 1000)
+
+# 2. 외부 도구 가져오기 및 llm에 등록
+tools = [ rag_search ] 
 
 # 랭그래프 상태 (커스텀)
 class AgentState(TypedDict):
