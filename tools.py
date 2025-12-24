41a8d7d307dd79c2eb26df6cb7f0672719a8fb5a
edu2ucoc <edu2.ucoc@gmail.com>
2025-12-24 13:32:29 +0900

    feat: tool과 rag 결합

--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@
 각종 툴을 모은 모듈
 '''
 from langchain_core.tools import tool
+from rag_store import search_stores
 
 @tool
 def rag_search(cate : str) -> str:
@@ -9,4 +10,5 @@ def rag_search(cate : str) -> str:
     특정 메뉴 카테고리 입력받아서 -> RAG 이용 -> 유사도 검색 -> 실제 식당정보등 반환    
     '''
     # RAG 검색
-    return "관련 식당 정보를 찾을 수 없습니다."
+    res = search_stores( cate ) # 기본 2개 설정
+    return res if res else "관련 식당 정보를 찾을 수 없습니다."
